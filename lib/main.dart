99d1c8b05593bb5c4ce47b5c334d8ffe08313f0a
acensito <acenforos@gmail.com>
2025-09-07 20:08:01 +0200

    Notificaciones cuando esta cerrada la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notifications/config/router/app_router.dart';
@@ -10,7 +11,8 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   // await NotificationsBloc.initializeFCM();
-
+  // Setup background message handler
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   // Firebase init
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -2,10 +2,18 @@ import 'package:equatable/equatable.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_notifications/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
+
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   final FirebaseMessaging messaging = FirebaseMessaging.instance;
